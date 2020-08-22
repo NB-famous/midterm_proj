c7177c36b608c602cc2b19e2f74a1909eafdf6e5
Nikko B <ncbadoles@gmail.com>
2020-08-22 22:05:12 +0000

    styling and partial footer added

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (id,username, email, password) 
+VALUES (1,'Alice', 'a@a.ca', 'password'),
+(2,'Kira', 'b@b.ca', 'password'),
+(3,'jonny', 'c@c.ca', 'password');
