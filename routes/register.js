66694e63f98af94f5aba614382a5a148b7090660
Julianna <julianna.j.lim@gmail.com>
2020-08-22 23:09:50 -0400

    cleaning up the register js function

--- routes/register.js
+++ routes/register.js
@@ -1,21 +1,16 @@
 const express = require('express');
+
 const router = express.Router();
 
 module.exports = (db) => {
-  console.log(db);
+  // console.log(db);
   router.get('/', (req, res) => {
     res.render('register');
   });
 
   router.post('/', (req, res) => {
+    // console.log(req.body);
     res.redirect('/');
-    return db.query(`
-        INSERT INTO users (username, email, password)
-        VALUES ($1, $2, $3)
-        RETURNING *
-      `, [db.username, db.email, db.password])
-      .then(res => res.rows[0])
-      .catch(err => null);
   });
   return router;
 };
