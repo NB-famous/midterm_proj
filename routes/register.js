76cd8c1f521dfbc6a250597ff4a0048dec98b401
Julianna <julianna.j.lim@gmail.com>
2020-08-22 22:50:06 -0400

    redirect on register to homepage no data added yet

--- routes/register.js
+++ routes/register.js
@@ -2,9 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
+  console.log(db);
   router.get('/', (req, res) => {
     res.render('register');
   });
 
+  router.post('/', (req, res) => {
+    res.redirect('/');
+    return db.query(`
+        INSERT INTO users (username, email, password)
+        VALUES ($1, $2, $3)
+        RETURNING *
+      `, [db.username, db.email, db.password])
+      .then(res => res.rows[0])
+      .catch(err => null);
+  });
   return router;
 };
+
+
+
