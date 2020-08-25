941213cf0d4bb0ce90fcbb752007e519f05bb6cf
Julianna <julianna.j.lim@gmail.com>
2020-08-25 16:40:51 -0400

    footer and front end fixes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,8 +2,9 @@ $(() => {
   $.ajax({
     method: "GET",
     url: "/api/users"
-  }).done(({users}) => {
-    for(user of users) {
+  }).done(({ users }) => {
+    console.log(users);
+    for (user of users) {
       $("<div>").text(user.email).appendTo($("div.added_users"));
     }
   });
