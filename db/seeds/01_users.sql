2945ea9236c0f077f324d20c41d829905006b490
Julianna <julianna.j.lim@gmail.com>
2020-08-26 15:41:07 -0400

    is_public became is_private in seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,10 +1,10 @@
 -- Users table seeds here (Example)
-INSERT INTO users (id,username, email, password) 
+INSERT INTO users (id,username, email, password)
 VALUES (1,'Alice', 'a@a.ca', 'password'),
 (2,'Kira', 'b@b.ca', 'password'),
 (3,'jonny', 'c@c.ca', 'password');
 
-INSERT INTO quizzes(owner_id, is_public, creation_date)
+INSERT INTO quizzes(owner_id, is_private, creation_date)
 VALUES (1, true, current_timestamp),
 (2, true, current_timestamp),
 (3, true, current_timestamp)
